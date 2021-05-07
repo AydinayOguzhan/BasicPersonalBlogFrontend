a06edc3b40e03984dd979886dbcd98c2bc8c1cc7
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2021-05-07 23:25:07 +0300

    Navbarda giriş yapmış kullanıcının bilgilerinin görünmesi ve üzerinden gidilecek menünün butonu eklendi

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/models/user/userModel';
 import { AuthService } from 'src/app/services/authService/auth.service';
+import { LocalStorageService } from 'src/app/services/localStorageService/local-storage.service';
+import { UserService } from 'src/app/services/userService/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,11 +12,19 @@ import { AuthService } from 'src/app/services/authService/auth.service';
 })
 export class NavbarComponent implements OnInit {
   logged:boolean = false
+  user: User
 
-  constructor(private authService:AuthService) { }
+  constructor(private authService:AuthService, private userService:UserService, private localStorageService:LocalStorageService) { }
 
   ngOnInit(): void {
     this.isLogged()
+    if (this.logged) {
+      this.getUserByEmail(this.localStorageService.getVariable("email"))
+      if (this.localStorageService.getVariable("id") == null) {
+        return null
+      }
+      // this.checkUserClaims(parseInt(this.localStorageService.getVariable("id")))
+    }
   }
 
   isLogged(){
@@ -25,5 +36,12 @@ export class NavbarComponent implements OnInit {
   logout(){
     this.authService.logout()
   }
+
+  getUserByEmail(email: string) {
+    this.userService.getByEmail(email).subscribe(response => {
+      this.user = response.data
+    })
+  }
+
 
 }
