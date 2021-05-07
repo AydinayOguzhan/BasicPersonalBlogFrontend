f3b5cc64928501a9ad717c907798a5dd20c8d9ed
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2021-05-07 23:42:53 +0300

    Admin kontrolü eklendi. Kullanıcı adminse navbarda admin panel özellikleri açılıyor

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { User } from 'src/app/models/user/userModel';
+import { UserOperationClaim } from 'src/app/models/user/userOperationClaim';
 import { AuthService } from 'src/app/services/authService/auth.service';
 import { LocalStorageService } from 'src/app/services/localStorageService/local-storage.service';
+import { UserOperationClaimService } from 'src/app/services/userOperationClaimService/user-operation-claim.service';
 import { UserService } from 'src/app/services/userService/user.service';
 
 @Component({
@@ -13,8 +15,11 @@ import { UserService } from 'src/app/services/userService/user.service';
 export class NavbarComponent implements OnInit {
   logged:boolean = false
   user: User
+  userOperationClaims: UserOperationClaim[]
+  ifAdmin: boolean
 
-  constructor(private authService:AuthService, private userService:UserService, private localStorageService:LocalStorageService) { }
+  constructor(private authService:AuthService, private userService:UserService, private localStorageService:LocalStorageService,
+    private userOperationClaimService:UserOperationClaimService) { }
 
   ngOnInit(): void {
     this.isLogged()
@@ -23,7 +28,7 @@ export class NavbarComponent implements OnInit {
       if (this.localStorageService.getVariable("id") == null) {
         return null
       }
-      // this.checkUserClaims(parseInt(this.localStorageService.getVariable("id")))
+      this.checkUserClaims(parseInt(this.localStorageService.getVariable("id")))
     }
   }
 
@@ -43,5 +48,18 @@ export class NavbarComponent implements OnInit {
     })
   }
 
+  
+  checkUserClaims(userId:number){
+    this.userOperationClaimService.checkUserClaims(userId).subscribe(response=>{
+      if (response.success) {
+        this.ifAdmin = response.success
+        console.log(response.success)
+      }else{
+        this.ifAdmin = response.success
+      }
+    },errorResponse=>{
+      console.log(errorResponse.error.message)
+    })
+  }
 
 }
---
+++ src/app/services/userOperationClaimService/user-operation-claim.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { ResponseModel } from 'src/app/models/responseModel';
+import { UserOperationClaim } from 'src/app/models/user/userOperationClaim';
+import { UserOperationClaimDetail } from 'src/app/models/user/userOperationClaimDetail';
+import { LocalStorageService } from '../localStorageService/local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserOperationClaimService {
+  apiUrl = "https://localhost:44335/api/"
+
+
+  constructor(private httpClient:HttpClient, private localStorageService:LocalStorageService) { }
+
+  getAll():Observable<ListResponseModel<UserOperationClaim>>{
+    let newPath = this.apiUrl + "useroperationclaims/getall"
+    return this.httpClient.get<ListResponseModel<UserOperationClaim>>(newPath)
+  }
+
+  checkUserClaims(userId:number):Observable<ResponseModel>{
+    let newPath = this.apiUrl + "useroperationclaims/checkifitsadmin?userId=" + userId
+    return this.httpClient.get<ResponseModel>(newPath)
+  }
+
+  getByUserId(userId:number):Observable<ListResponseModel<UserOperationClaim>>{
+    let newPath = this.apiUrl + "useroperationclaims/getbyuserid?userId=" + userId
+    return this.httpClient.get<ListResponseModel<UserOperationClaim>>(newPath)
+  }
+
+  delete(userOperationClaim:UserOperationClaim):Observable<ResponseModel>{
+    let newPath = this.apiUrl + "useroperationclaims/delete"
+    return this.httpClient.post<ResponseModel>(newPath,userOperationClaim) 
+  }
+
+  add(userOperationClaim:UserOperationClaim):Observable<ResponseModel>{
+    let newPath = this.apiUrl + "useroperationclaims/add"
+    return this.httpClient.post<ResponseModel>(newPath,userOperationClaim)
+  }
+
+  getUserClaimsDetailsByUserId(userId:number):Observable<ListResponseModel<UserOperationClaimDetail>>{
+    let newPath = this.apiUrl + "useroperationclaims/getclaimsdetailsbyuserid?userId=" + userId
+    return this.httpClient.get<ListResponseModel<UserOperationClaimDetail>>(newPath)
+  }
+}
